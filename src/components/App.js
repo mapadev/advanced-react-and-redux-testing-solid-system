6e5f2413eaa25abbba03fd0d1b570da6e8d79ad1
gogolab <m4ttek@gmail.com>
2018-10-20 16:03:10 +0200

    added app and comment components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
+
+export default function() {
+    return (
+        <div>
+            <CommentBox />
+            <CommentList />
+        </div>
+    );
+}
