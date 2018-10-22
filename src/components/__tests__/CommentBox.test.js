7e33f6cf71142b7972ecf39d9e284879fad452fe
gogolab <m4ttek@gmail.com>
2018-10-22 12:15:11 +0200

    added tests for input change and form submit handlers

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,3 +17,29 @@ it("has a textarea and a button", () => {
     expect(wrapped.find("textarea").length).toEqual(1);
     expect(wrapped.find("button").length).toEqual(1);
 });
+
+it("has a text area that user can type in", () => {
+    wrapped
+        .find("textarea")
+        .simulate("change", { target: { value: "new comment" } });
+
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("when form is submitted, comment box gets emptied", () => {
+    wrapped
+        .find("textarea")
+        .simulate("change", { target: { value: "new comment" } });
+
+    wrapped.update();
+
+    wrapped.find("form").simulate("submit");
+
+    // TODO - check if preventDefault() was called.
+
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
