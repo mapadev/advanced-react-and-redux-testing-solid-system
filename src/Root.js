b6fd66d7296053e7d6f8925180c41721a1f4cd65
gogolab <m4ttek@gmail.com>
2018-10-23 13:31:12 +0200

    refactored Root to allow testing components which require store access

--- src/Root.js
+++ src/Root.js
@@ -5,9 +5,22 @@ import { createStore } from "redux";
 import reducers from "reducers";
 
 function Root(props) {
+    const initialState = props.initialState || {};
+
     return (
-        <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+        <Provider store={createStore(reducers, initialState)}>
+            {props.children}
+        </Provider>
     );
 }
+
+// // ALTER: Default initialState using destructuring:
+// function Root({children, initialState = {}}) {
+//     return (
+//         <Provider store={createStore(reducers, initialState)}>
+//             {children}
+//         </Provider>
+//     );
+// }
 
 export default Root;
