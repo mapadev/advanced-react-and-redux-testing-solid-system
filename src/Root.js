249bffc49fadd5b1ce79d8aa3fad479faee7a9ec
gogolab <m4ttek@gmail.com>
2018-10-25 13:11:36 +0200

    added async and stateValidator middlewares

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
+
+// import reduxPromise from "redux-promise";
+import async from "middlewares/async";
+
+import stateValidator from "middlewares/stateValidator";
 
 import reducers from "reducers";
 
@@ -11,7 +15,7 @@ function Root(props) {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async, stateValidator)
     );
 
     return <Provider store={store}>{props.children}</Provider>;
